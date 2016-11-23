a6ee7ad55f3ca9a182ac6aba162fb8f5cc6cd60a
zaturnvalley <zacharycmason@gmail.com>
2016-11-23 11:58:16 -0800

    created book-list component

---
+++ src/components/book-list.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+      </ul>
+    );
+  }
+}
