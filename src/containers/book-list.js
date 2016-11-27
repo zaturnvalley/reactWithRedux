685fa103eee0441624d6df8780ea8eb8208913f4
zaturnvalley <zacharycmason@gmail.com>
2016-11-26 19:22:46 -0800

    mapStateToProps func returns state.books

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -19,4 +19,12 @@ export default class BookList extends Component {
       </ul>
     );
   }
+}
+
+function mapStateToProps(state) {
+  // Whatever is treturned will show up as props
+  // inside of BookList
+  return {
+    books: state.books
+  };
 }
