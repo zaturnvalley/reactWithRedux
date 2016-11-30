3cc69aa873aa781d48ae7880400d84b0d5e3f2cf
zaturnvalley <zacharycmason@gmail.com>
2016-11-30 14:57:32 -0800

    omitted console log in action, comment added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book){
-  console.log('A Book has been selected:', book.title);
+  // selectBook is an ActionCreator that
+  // needs to return an action, an object
+  // with a type property
+  return {
+
+  };
 }
