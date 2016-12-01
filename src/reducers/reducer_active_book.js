6b21fb4a90d0ba1f90ef98af528c1978338b6169
zaturnvalley <zacharycmason@gmail.com>
2016-12-01 11:34:58 -0800

    if case BOOK_SELECTED return action.payload

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,4 +1,9 @@
+// state arg is not application state,
+// only the state this reducer is responsible for
 export default function(state, action) {
-
-
+  switch(action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+  return state;
 }
