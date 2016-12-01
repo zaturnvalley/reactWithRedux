d683cbe1c25266031906dc89d03e7fac9ee096ab
zaturnvalley <zacharycmason@gmail.com>
2016-12-01 11:36:39 -0800

    if arg comes in undefined, set state to null

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,9 +1,12 @@
 // state arg is not application state,
 // only the state this reducer is responsible for
-export default function(state, action) {
+
+//if arg comes in undefined, set state to null
+export default function(state = null, action) {
   switch(action.type) {
     case 'BOOK_SELECTED':
       return action.payload;
   }
+  //if no book selected, return state on intial bootup
   return state;
 }
