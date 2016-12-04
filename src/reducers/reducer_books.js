ca13c4b49accaadd80c9bbc8e62682453587146d
zaturnvalley <zacharycmason@gmail.com>
2016-12-03 17:10:27 -0800

    added pages to book reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'East of Eden' },
-    { title: 'Brothers Karamazov' },
-    { title: 'Kafka on the Shore' },
-    { title: 'A Wild Sheep Chase' }
+    { title: 'East of Eden', pages: 405 },
+    { title: 'Brothers Karamazov', pages: 800 },
+    { title: 'Kafka on the Shore', pages: 434 },
+    { title: 'A Wild Sheep Chase', pages: 302 }
   ]
 }
