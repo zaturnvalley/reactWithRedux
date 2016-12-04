80f8e14be5d51757460280ce6e9dea12fc6102f5
zaturnvalley <zacharycmason@gmail.com>
2016-12-03 21:03:37 -0800

    added about section on reducer obj

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,46 @@
 export default function() {
   return [
-    { title: 'East of Eden', pages: 405 },
-    { title: 'Brothers Karamazov', pages: 800 },
-    { title: 'Kafka on the Shore', pages: 434 },
-    { title: 'A Wild Sheep Chase', pages: 302 }
+    { title: 'East of Eden', 
+      pages: 601, 
+      about: `East of Eden is a novel by Nobel Prize winner John Steinbeck, published in September 1952. 
+            Often described as Steinbeck\'s most ambitious novel, East of Eden brings to life the intricate details of two families, 
+            the Trasks and the Hamiltons, and their interwoven stories. The novel was originally addressed to Steinbeck\'s young sons, 
+            Thom and John (then 6 and 4 years old, respectively). Steinbeck wanted to describe the Salinas Valley for them in detail: 
+            the sights, sounds, smells, and colors.
+            The Hamilton family in the novel is said to be based on the real-life family of Samuel Hamilton, Steinbeck's maternal grandfather. 
+            A young John Steinbeck also appears briefly in the novel as a minor character.
+            According to his third and last wife, Elaine, Steinbeck considered it his magnum opus. Steinbeck stated about East of Eden: 
+            "It has everything in it I have been able to learn about my craft or profession in all these years." He further claimed: 
+            "I think everything else I have written has been, in a sense, practice for this."`
+    },
+    { title: 'The Brothers Karamazov', 
+      pages: 824,
+      about: `The Brothers Karamazov, also translated as The Karamazov Brothers, is the final novel by the Russian author Fyodor Dostoyevsky. 
+            Dostoyevsky spent nearly two years writing The Brothers Karamazov, which was published as a serial in The Russian Messenger from 
+            January 1879 to November 1880. The author died less than four months after its publication.
+            The Brothers Karamazov is a passionate philosophical novel set in 19th-century Russia, that enters deeply into the ethical debates of God, 
+            free will, and morality. It is a spiritual drama of moral struggles concerning faith, doubt, judgment, and reason, set against a modernizing 
+            Russia, with a plot which revolves around the subject of patricide. Dostoyevsky composed much of the novel in Staraya Russa, which inspired 
+            the main setting. Since its publication, it has been acclaimed as one of the supreme achievements in world literature.` 
+    },
+    { title: 'Kafka on the Shore', 
+      pages: 480,
+      about: `A 2002 novel by Japanese author Haruki Murakami. Comprising two distinct but interrelated plots, the narrative runs back and forth between 
+            both plots, taking up each plotline in alternating chapters. The odd chapters tell the 15-year-old Kafka's story as he runs away from his father's 
+            house to escape an Oedipal curse and to embark upon a quest to find his mother and sister. After a series of adventures, he finds shelter in a quiet, 
+            private library in Takamatsu, run by the distant and aloof Miss Saeki and the intelligent and more welcoming Oshima. There he spends his days reading 
+            the unabridged Richard Francis Burton translation of One Thousand and One Nights and the collected works of Natsume Sōseki until the police begin inquiring 
+            after him in connection with a brutal murder. The even chapters tell Nakata's story. Due to his uncanny abilities, he has found part-time work in his old age 
+            as a finder of lost cats (notably, Murakami's earlier work The Wind-Up Bird Chronicle also involves searching for a lost cat). The case of one particular 
+            lost cat puts him on a path that ultimately takes him far away from his home, ending up on the road for the first time in his life. He befriends a truck driver 
+            named Hoshino, who takes him on as a passenger in his truck and soon becomes very attached to the old man.`
+    },
+    { title: 'A Wild Sheep Chase', 
+      pages: 353,
+      about: `is the third novel by Japanese author Haruki Murakami. First published in Japan in 1982, it was translated into English in 1989. It is an independent sequel to 
+            Pinball, 1973, and the third book in the so-called "Trilogy of the Rat". It won the 1982 Noma Literary Newcomer's Prize.
+            In A Wild Sheep Chase, Murakami blends elements of American and English literature with Japanese contexts, exploring post-WWII Japanese cultural identity. 
+            The book is part mystery and part magical realism with a postmodern twist.` 
+    }
   ]
 }
